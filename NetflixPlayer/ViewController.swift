9ad21c1ee85287dec595ecd84f4e7a0a24e559cb
Pushpendra Kumar <sainipushpendra51@gmail.com>
2023-07-09 14:35:07 +0530

    Initial commit with improved feature of timer and rotation of the screen

---
+++ NetflixPlayer/ViewController.swift
@@ -0,0 +1,184 @@
+//
+//  ViewController.swift
+//  NetflixPlayer
+//
+//  Created by Pushpendra on 29/06/23.
+//
+
+import UIKit
+import AVKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var videoPlayer: UIView!
+    @IBOutlet weak var videoPlayerHeight: NSLayoutConstraint!
+    @IBOutlet weak var viewControll: UIView!
+    @IBOutlet weak var stackCtrView: UIStackView!
+    @IBOutlet weak var img10SecBack: UIImageView! {
+        didSet {
+            self.img10SecBack.isUserInteractionEnabled = true
+            self.img10SecBack.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(onTap10SecBack)))
+        }
+    }
+    @IBOutlet weak var imgPlay: UIImageView! {
+        didSet {
+            self.imgPlay.isUserInteractionEnabled = true
+            self.imgPlay.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(onTapPlayPause)))
+        }
+    }
+    @IBOutlet weak var img10SecFor: UIImageView! {
+        didSet {
+            self.img10SecFor.isUserInteractionEnabled = true
+            self.img10SecFor.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(onTap10SecNext)))
+        }
+    }
+    
+    @IBOutlet weak var lbCurrentTime: UILabel!
+    @IBOutlet weak var lbTotalTime: UILabel!
+    @IBOutlet weak var seekSlider: UISlider! {
+        didSet {
+            self.seekSlider.addTarget(self, action: #selector(onTapToSlide), for: .valueChanged)
+        }
+    }
+    
+    
+    let videoURL = "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4"
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        self.setVideoPlayer()
+    }
+
+    private var player : AVPlayer? = nil
+    private var playerLayer : AVPlayerLayer? = nil
+    
+    private func setVideoPlayer() {
+        guard let url = URL(string: videoURL) else { return }
+        
+        if self.player == nil {
+            self.player = AVPlayer(url: url)
+            self.playerLayer = AVPlayerLayer(player: self.player)
+            self.playerLayer?.videoGravity = .resizeAspectFill
+            self.playerLayer?.frame = self.videoPlayer.bounds
+            self.playerLayer?.addSublayer(self.viewControll.layer)
+            if let playerLayer = self.playerLayer {
+                self.view.layer.addSublayer(playerLayer)
+            }
+            self.player?.play()
+        }
+        self.setObserverToPlayer()
+    }
+    
+    private var windowInterface : UIInterfaceOrientation? {
+        return self.view.window?.windowScene?.interfaceOrientation
+    }
+    
+    override func willTransition(to newCollection: UITraitCollection, with coordinator: UIViewControllerTransitionCoordinator) {
+        super.willTransition(to: newCollection, with: coordinator)
+        guard let windowInterface = self.windowInterface else { return }
+        if #available(iOS 16.0, *) {
+            if windowInterface.isPortrait ==  true {
+                self.videoPlayerHeight.constant = 300
+            } else {
+                self.videoPlayerHeight.constant = self.view.layer.bounds.width
+            }
+            print(self.videoPlayerHeight.constant)
+            DispatchQueue.main.asyncAfter(deadline: .now() + 0.1, execute: {
+                self.playerLayer?.frame = self.videoPlayer.bounds
+            })
+        }
+    }
+    
+    private var timeObserver : Any? = nil
+    private func setObserverToPlayer() {
+        let interval = CMTime(seconds: 0.3, preferredTimescale: CMTimeScale(NSEC_PER_SEC))
+        timeObserver = player?.addPeriodicTimeObserver(forInterval: interval, queue: DispatchQueue.main, using: { elapsed in
+            self.updatePlayerTime()
+        })
+    }
+    
+    private func updatePlayerTime() {
+        guard let currentTime = self.player?.currentTime() else { return }
+        guard let duration = self.player?.currentItem?.duration else { return }
+        
+        let currentTimeInSecond = CMTimeGetSeconds(currentTime)
+        let durationTimeInSecond = CMTimeGetSeconds(duration)
+        
+        if self.isThumbSeek == false {
+            self.seekSlider.value = Float(currentTimeInSecond/durationTimeInSecond)
+        }
+        
+        let value = Float64(self.seekSlider.value) * CMTimeGetSeconds(duration)
+        
+        var hours = value / 3600
+        var mins =  (value / 60).truncatingRemainder(dividingBy: 60)
+        var secs = value.truncatingRemainder(dividingBy: 60)
+        var timeformatter = NumberFormatter()
+        timeformatter.minimumIntegerDigits = 2
+        timeformatter.minimumFractionDigits = 0
+        timeformatter.roundingMode = .down
+        guard let hoursStr = timeformatter.string(from: NSNumber(value: hours)), let minsStr = timeformatter.string(from: NSNumber(value: mins)), let secsStr = timeformatter.string(from: NSNumber(value: secs)) else {
+            return
+        }
+        self.lbCurrentTime.text = "\(hoursStr):\(minsStr):\(secsStr)"
+        
+        hours = durationTimeInSecond / 3600
+        mins = (durationTimeInSecond / 60).truncatingRemainder(dividingBy: 60)
+        secs = durationTimeInSecond.truncatingRemainder(dividingBy: 60)
+        timeformatter = NumberFormatter()
+        timeformatter.minimumIntegerDigits = 2
+        timeformatter.minimumFractionDigits = 0
+        timeformatter.roundingMode = .down
+        guard let hoursStr = timeformatter.string(from: NSNumber(value: hours)), let minsStr = timeformatter.string(from: NSNumber(value: mins)), let secsStr = timeformatter.string(from: NSNumber(value: secs)) else {
+            return
+        }
+        self.lbTotalTime.text = "\(hoursStr):\(minsStr):\(secsStr)"
+    }
+    
+    @objc private func onTap10SecNext() {
+        guard let currentTime = self.player?.currentTime() else { return }
+        let seekTime10Sec = CMTimeGetSeconds(currentTime).advanced(by: 10)
+        let seekTime = CMTime(value: CMTimeValue(seekTime10Sec), timescale: 1)
+        self.player?.seek(to: seekTime, completionHandler: { completed in
+            
+        })
+    }
+    
+    @objc private func onTap10SecBack() {
+        guard let currentTime = self.player?.currentTime() else { return }
+        let seekTime10Sec = CMTimeGetSeconds(currentTime).advanced(by: -10)
+        let seekTime = CMTime(value: CMTimeValue(seekTime10Sec), timescale: 1)
+        self.player?.seek(to: seekTime, completionHandler: { completed in
+            
+        })
+    }
+    
+    @objc private func onTapPlayPause() {
+        if self.player?.timeControlStatus == .playing {
+            self.imgPlay.image = UIImage(systemName: "pause.circle")
+            self.player?.pause()
+        } else {
+            self.imgPlay.image = UIImage(systemName: "play.circle")
+            self.player?.play()
+        }
+    }
+    
+    private var isThumbSeek : Bool = false
+    @objc private func onTapToSlide() {
+        self.isThumbSeek = true
+        guard let duration = self.player?.currentItem?.duration else { return }
+        let value = Float64(self.seekSlider.value) * CMTimeGetSeconds(duration)
+        if value.isNaN == false {
+            let seekTime = CMTime(value: CMTimeValue(value), timescale: 1)
+            self.player?.seek(to: seekTime, completionHandler: { completed in
+                if completed {
+                    self.isThumbSeek = false
+                }
+            })
+        }
+    }
+}
+
--- README.md
+++ README.md
@@ -1,1 +1,39 @@
-# avplayer-swift-part-3-ios
+# AVPlayer: Play, Pause, Seek, and Time Control in iOS with Swift
+
+[![Custom AV Player](https://github.com/pushpendra996/avplayer-swift-part-1-ios/blob/main/Custom%20AV%20Player%20in%20Swift%20iOS.png?raw=true)](https://youtu.be/naMfH2nryvQ)
+
+
+AVPlayer is a powerful framework in iOS that allows developers to handle video playback with ease. In this GitHub repository, we provide a comprehensive guide and code examples for implementing essential control functionalities for AVPlayer, including play, pause, seek, and time control. By following the instructions and code snippets provided here, you can create a robust and interactive video player in your iOS app.
+
+## Features Covered
+
+* Play and pause video playback with AVPlayer
+* Seek forward and backward within the video timeline
+* Implement seek control via a seekbar/slider
+* Display current playback time and total duration of the video
+
+
+
+## Video Tutorial
+
+For a detailed explanation and demonstration of the device rotation control implementation, make sure to watch the corresponding video tutorial on our YouTube channel. The tutorial covers the code implementation and provides valuable insights to help you incorporate this feature into your own video player.
+
+[Video Playlist for AvPlayer](https://www.youtube.com/watch?v=naMfH2nryvQ&list=PL2Sk-NhO3icHtmBTtg70eeOUug4CEzaSm)
+
+## Contributions
+
+We welcome contributions from the community to improve this project further. If you have any suggestions, bug fixes, or additional features you'd like to add, feel free to submit a pull request.
+
+## Stay Connected
+
+To stay updated with the upcoming parts of the Custom AV Player Series and other exciting tutorials, don't forget to subscribe to our YouTube channel and follow us on social media.
+
+* YouTube: [YouTube Channel](https://www.youtube.com/PushpendraSaini)
+* Linked In: [Linkedin](https://www.linkedin.com/in/pushpendra-saini/)
+
+
+We hope you enjoy implementing device rotation control in your custom AV player with our Custom AV Player Series. Stay tuned for the next parts, where we'll explore more advanced topics and features.
+
+## License
+
+This project is licensed under the MIT License.
